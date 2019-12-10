eaf43fa5a892bb8517093d4383f22025c6e4dce5
carolinadutras <carolina.dutras@gmail.com>
2019-12-10 17:04:22 -0300

    toggle etapas

--- javaScript/mercado.js
+++ javaScript/mercado.js
@@ -1,9 +1,19 @@
 
-function myFunction() {
-    var x = document.getElementById("myDIV");
+function mostraMercado() {
+    let x = document.getElementById("mostraDiv");
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
       x.style.display = "none";
+    }
+  }
+
+
+  function myFunction() {
+    let y = document.getElementById("mostraPreco");
+    if (y.style.display === "none") {
+      y.style.display = "block";
+    } else {
+      y.style.display = "none";
     }
   }
