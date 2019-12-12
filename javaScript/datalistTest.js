4fcf504f0815976a08c47fef6544d2d2b0445ff4
Gabriel Pinheiro Bartholomeu <gabriel_bartholomeu@hotmail.com>
2019-12-12 14:42:40 -0300

    Princeso Divo. JSON Datalista dynamics loading

--- javaScript/datalistTest.js
+++ javaScript/datalistTest.js
@@ -11,13 +11,12 @@ request.onreadystatechange = function(response) {
     if (request.status === 200) {
       // Parse the JSON
       var jsonOptions = JSON.parse(request.responseText);
-  
       // Loop over the JSON array.
       jsonOptions.forEach(function(item) {
         // Create a new <option> element.
         var option = document.createElement('option');
         // Set the value using the item in the JSON array.
-        option.value = item;
+        option.value = item.descricao;
         // Add the <option> element to the <datalist>.
         dataList.appendChild(option);
       });
@@ -35,5 +34,6 @@ request.onreadystatechange = function(response) {
 input.placeholder = "Loading options...";
 
 // Set up and make the request.
-request.open("GET","listaMercado.json",true);
+request.open("GET","http://127.0.0.1:8080/listaMercado2.json",true);
+
 request.send();
