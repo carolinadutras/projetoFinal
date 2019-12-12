a1ef1085c796743ba02e711c50ccaf799a7521b8
Gabriel Pinheiro Bartholomeu <gabriel_bartholomeu@hotmail.com>
2019-12-12 09:49:34 -0300

    Revert de modificações não feitas por mim

--- javaScript/datalistTest.js
+++ javaScript/datalistTest.js
@@ -6,12 +6,34 @@ var input = document.getElementById('ajax');
 var request = new XMLHttpRequest();
 
 // Handle state changes for the request.
-request.onreadystatechange = function(){
-    if(request.status == 200 && request.readyState == 4){
-      txt = request.responseText;
+request.onreadystatechange = function(response) {
+  if (request.readyState === 4) {
+    if (request.status === 200) {
+      // Parse the JSON
+      var jsonOptions = JSON.parse(request.responseText);
+  
+      // Loop over the JSON array.
+      jsonOptions.forEach(function(item) {
+        // Create a new <option> element.
+        var option = document.createElement('option');
+        // Set the value using the item in the JSON array.
+        option.value = item;
+        // Add the <option> element to the <datalist>.
+        dataList.appendChild(option);
+      });
+      
+      // Update the placeholder text.
+      input.placeholder = "e.g. datalist";
+    } else {
+      // An error occured :(
+      input.placeholder = "Couldn't load datalist options :(";
     }
-  };
+  }
+};
+
+// Update the placeholder text.
+input.placeholder = "Loading options...";
 
 // Set up and make the request.
-request.open("GET","/Users/bartholomeu/Documents/Gits/projetoFinal/API/listaMercado.json",true);
+request.open("GET","listaMercado.json",true);
 request.send();
